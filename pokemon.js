cf533c7f240c45b189c5325c712d357d20ae7dc9
Tatiana Chaykina <104149044+TanyaSeagull@users.noreply.github.com>
2024-08-10 12:03:37 +0300

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,10 @@
+//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+//GEN1 -> 1 to 151
+//GEN2 -> 151 to 251
+//GEN3 -> 252 to 386
+
+const mainBox = document.querySelector('#container');
+const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const gen3 = document.querySelector('.Gen3');
+const gen1 = document.querySelector('.Gen1');
+const gen2 = document.querySelector('.Gen2');
